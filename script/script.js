e56aab99ed2ea27a6c5cac72b794f5a09a01ffaa
libewa <67926131+libewa@users.noreply.github.com>
2023-11-18 10:54:59 +0100

    stop removing my stuff nonline

--- script/script.js
+++ script/script.js
@@ -3,6 +3,10 @@ mapkit.addEventListener("configuration-change", function(event) {
     switch (event.status) {
     case "Initialized":
         // MapKit JS initializes and configures.
+        new mapkit.Map(
+            "map"
+        );
+        console.log("MapKit JS init");
         break;
     case "Refreshed":
         // The MapKit JS configuration updates.
