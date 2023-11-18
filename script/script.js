4c995c63fccbce6c03753dc10468f02dbeac9946
Nils-nonline <nils-nonline@web.de>
2023-11-18 10:37:54 +0100

    added script.js to script/script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
-
-
 document.write(JSON.stringify(data))
+
+mapkit.init({ authorizationCallback: function(done) { done("your-token"); }, ... });
+mapkit.addEventListener("configuration-change", function(event) {
+    switch (event.status) {
+    case "Initialized":
+        // MapKit JS initializes and configures.
+        break;
+    case "Refreshed":
+        // The MapKit JS configuration updates.
+        break;
+    }
+});
